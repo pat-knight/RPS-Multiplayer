07287bd2fbc1d5ed79aa53fd9e5f827840e33303
Patrick Knight <ptknight00@gmail.com>
2018-01-14 11:47:25 -0600

    app skeleton

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAP1uWVNAkf27MDW91pfy1K83-PoNmLNns",
+    authDomain: "rps-multiplayer-26026.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-26026.firebaseio.com",
+    projectId: "rps-multiplayer-26026",
+    storageBucket: "rps-multiplayer-26026.appspot.com",
+    messagingSenderId: "703791195712"
+  };
+  
+  firebase.initializeApp(config);
